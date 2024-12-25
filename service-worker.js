005deb5f6cb99452588c88651a3c52450904ae72
09-01-16 <mkim7171@gmail.com>
2024-12-26 02:05:02 +0800

    dasd

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/main.js',
+                '/dowoon.jpg',
+                'pfp.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
